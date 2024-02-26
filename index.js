9cfb3e95be6f4eff8887ad20518b037730b04620
Emily Consuelo Mena Terrero <135460520+emena2@users.noreply.github.com>
2024-02-26 19:42:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const http = require('http');
+var semaphore = require('semaphore')(2);
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Contador de personas en cola
+let cola = 0;
+let turnoActual = 1;
+
+//Lectura archivo html - con esto es posible captar el nombre que el usuario escribe
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	// Enviar información inicial al cliente
+	socket.emit('init', { colaActual: cola });
+	// Tomar un turno (aquí se implementa el semáforo, permitiendo que sólo dos personas a la vez puedan ingresar a la cola)
+
+	socket.on('tomarTurno', (nombre) => {
+		semaphore.take(() => {
+			console.log('Añadiendo usuarios a la cola, espere...');
+			try {
+				if (nombre.trim() == '') throw 'vacío';
+				let x = parseInt(nombre);
+				if (x != NaN) throw `${nombre} no es un nombre`;
+			} catch (err) {
+				alert(err);
+			}
+			{
+				cola++;
+				const turnoUsuario = turnoActual++;
+
+				// Emitir eventos a todos los clientes conectados
+				io.emit('actualizarTurno', {
+					colaActual: cola,
+					turnoUsuario: turnoUsuario,
+				});
+
+				console.log(`Nuevo turno: ${turnoUsuario} - ${nombre}`);
+			}
+
+			setTimeout(() => {
+				console.log('Añadido a la cola!');
+				semaphore.leave();
+			}, 5000);
+		});
+	});
+});
+
+const PORT = process.env.PORT || 2600;
+
+server.listen(PORT, () => {
+	console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+});
